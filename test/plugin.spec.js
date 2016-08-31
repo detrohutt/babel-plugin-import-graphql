e7f1a915168f6edd0b508b3a485377ccf61e40e0
zVictor <victor0110@msn.com>
2016-08-31 13:57:45 +0200

    add plugin specs

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -3,26 +3,37 @@ import * as babel from 'babel-core';
 
 describe('Babel Inline Import - Plugin', () => {
   describe('Babel Plugin', () => {
-    it('transforms the relative path into an absolute path', () => {
-      const targetRequire = `${process.cwd()}/some/example.js`;
-      const transformedCode = babel.transform("import SomeExample from '/some/example.js';", {
+    it('transforms the import statement into a variable with the intended content', () => {
+      const transformedCode = babel.transform("import SomeExample from './fixtures/example.raw';", {
+        filename: __filename,
         plugins: [BabelInlineImportPlugin]
       });
 
-      expect(transformedCode.code).to.contain(targetRequire);
+      expect(transformedCode.code).to.equal(`var SomeExample = 'a raw content\\n';`);
     });
 
-    it('transforms the relative path into an absolute path with the configured root-path', () => {
-      const targetRequire = `some/custom/root/some/example.js`;
-      const transformedCode = babel.transform("import SomeExample from '/some/example.js';", {
+    it('accepts different extensions', () => {
+      const transformedCode = babel.transform("import SomeExample from './fixtures/example.py';", {
+        filename: __filename,
         plugins: [[
           BabelInlineImportPlugin, {
-            rootPathSuffix: 'some/custom/root'
+            extensions: [
+              '.py'
+            ]
           }
         ]]
       });
 
-      expect(transformedCode.code).to.contain(targetRequire);
+      expect(transformedCode.code).to.equal(`var SomeExample = 'print 1 + 1\\n';`);
+    });
+
+    it('doesn`t act when importing with destructuring', () => {
+      const transformedCode = babel.transform("import { SomeExample, AnotherExample } from './fixtures/example.raw';", {
+        filename: __filename,
+        plugins: [BabelInlineImportPlugin]
+      });
+
+      expect(transformedCode.code).to.equal("import { SomeExample, AnotherExample } from './fixtures/example.raw';");
     });
   });
 });
