d398b1e4faaad76848176e0ab54c289e36043967
zVictor <victor0110@msn.com>
2016-08-31 13:58:03 +0200

    implement plugin

--- plugin/index.js
+++ plugin/index.js
@@ -3,33 +3,26 @@ import BabelRootImportHelper from './helper';
 export default function({ types: t }) {
   class BabelRootImport {
     constructor() {
-      const that = this;
       return {
         visitor: {
           ImportDeclaration(path, state) {
             const givenPath = path.node.source.value;
+            const reference = state && state.file && state.file.opts.filename;
+            const extensions = state && state.opts && state.opts.extensions;
 
-            var rootPathSuffix = state && state.opts && typeof state.opts.rootPathSuffix === 'string' ?
-            '/' + state.opts.rootPathSuffix.replace(/^(\/)|(\/)$/g, '') :
-                '';
+            if(BabelRootImportHelper.shouldBeInlined(givenPath, extensions)) {
+              const id = path.node.specifiers[0].local.name;
+              const content = BabelRootImportHelper.getContents(givenPath, reference);
+              const variable = t.variableDeclarator(t.identifier(id), t.stringLiteral(content));
 
-            if(BabelRootImportHelper().hasRoot(givenPath)) {
-              path.node.source.value = BabelRootImportHelper().transformRelativeToRootPath(givenPath, rootPathSuffix);
+              path.replaceWith(
+                t.variableDeclaration('const', [variable])
+              );
             }
           }
         }
       };
     }
-  }
-
-  // Here's we detect the use of Meteor and send a dummy plugin.
-  // That's because, Meteor already do this for us.
-  //  global.meteorBabelHelpers is something we can see when
-  //  running inside Meteor.
-  if (global.meteorBabelHelpers) {
-    return {
-      visitor: {}
-    };
   }
 
   return new BabelRootImport();
