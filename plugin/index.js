dedfcd5bdfc0587ef56f4d509732f7b8e7034a58
Alexander Roberts <detrohutt@gmail.com>
2017-07-21 23:16:24 -0500

    Major rewrite with possible removal of Meteor support
    * Remove section left over from inline-import project concerning Meteor.. I didn't really understand how it worked so this project may not work with Meteor anymore, if it ever did. There is another solution available for Meteor so I'm not worried about this, and will bump to v2.0.0 just in case.
    * Remove some custom-made functions in favor of similar functions provided by node, eliminating need for helper.js
    * Remove dependence on "resolve"
    * Remove some unused/undocumented code paths and features like customizable extensions left over from inline-import
    * Refactor to a more object-oriented approach for better readability
    * Filter out non-import lines when processing GraphQL imports(will hopefully fix problems with whitespace-only lines)

--- plugin/index.js
+++ plugin/index.js
@@ -1,70 +1,55 @@
 import path from 'path'
 import os from 'os'
+import fs from 'fs'
 import gql from 'graphql-tag'
 import { parse } from 'babylon'
-import BabelInlineImportHelper from './helper'
-
-const filterFn = line =>
-	!(line[0] === '#' && line.slice(1).split(' ')[0] === 'import') &&
-	!(line === '')
-
-const stripImports = source => source.split(os.EOL).filter(filterFn).join(os.EOL)
-
-const expandImports = (source, resolvePath, reference) => {
-	reference = path.dirname(reference)
-	const queryAST = gql`${stripImports(source)}`
-	const lines = source.split(os.EOL)
-	let defs = queryAST.definitions
-	lines.some(line => {
-		if (line[0] === '#' && line.slice(1).split(' ')[0] === 'import') {
-			const importFile = line.slice(1).split(' ')[1].slice(1, -1)
-			const fragmentAST = gql`${BabelInlineImportHelper.getContents(importFile, path.resolve(reference, resolvePath))}`
-			defs = [...defs, ...fragmentAST.definitions]
-		}
-		return line.length !== 0 && line[0] !== '#'
-	})
-	return { ...queryAST, ...{ definitions: defs } }
-}
 
 export default ({ types: t }) => ({
 	visitor: {
 		ImportDeclaration: {
-			exit(path, state) {
-				const {
-					shouldBeInlined,
-					hasRoot,
-					transformRelativeToRootPath,
-					getContents
-				} = BabelInlineImportHelper
+			exit(curPath, state) {
+				const importPath = curPath.node.source.value
+				if (importPath.endsWith('.graphql') || importPath.endsWith('.gql')) {
+					const query = createQuery(importPath, state.file.opts.filename)
+					query.processFragments()
+					query.parse()
+					curPath.replaceWith(t.variableDeclaration('const', [buildInlineVariable(query.ast)]))
 
-				const givenPath = path.node.source.value
-				let reference = state && state.file && state.file.opts.filename
-				const extensions = state && state.opts && state.opts.extensions
-
-				if (BabelInlineImportHelper.shouldBeInlined(givenPath, extensions)) {
-					if (path.node.specifiers.length > 1) {
-						throw new Error(
-							`Destructuring inlined import is not allowed. Check the import statement for '${givenPath}'`
+					function buildInlineVariable (graphqlAST) {
+						const babelAST = parse('const obj = ' + JSON.stringify(graphqlAST))
+						const objExp = babelAST.program.body[0].declarations[0].init
+						return t.variableDeclarator(
+							t.identifier(curPath.node.specifiers[0].local.name),
+							t.objectExpression(objExp.properties)
 						)
 					}
-
-					// Here we detect the use of Meteor by checking global.meteorBabelHelpers
-					if (global.meteorBabelHelpers && hasRoot(reference)) {
-						reference = transformRelativeToRootPath(reference)
-					}
-
-					const content = getContents(givenPath, reference)
-					const graphqlAST = expandImports(content, givenPath, reference)
-					const babelAST = parse('const obj = ' + JSON.stringify(graphqlAST))
-					const objExp = babelAST.program.body[0].declarations[0].init
-					const variable = t.variableDeclarator(
-						t.identifier(path.node.specifiers[0].local.name),
-						t.objectExpression(objExp.properties)
-					)
-
-					path.replaceWith(t.variableDeclaration('const', [variable]))
 				}
 			}
 		}
 	}
 })
+
+function createQuery (queryPath, babelPath) {
+	const absPath = path.resolve(path.dirname(babelPath), queryPath)
+	const source = fs.readFileSync(absPath).toString()
+	let ast = null
+	let fragmentDefs = []
+
+	return {
+		processFragments () {
+			const imports = source.split(os.EOL).filter(line => line.startsWith('#import'))
+			imports.forEach(statement => {
+				const fragmentPath = statement.split(' ')[1].slice(1, -1)
+				const absFragmentPath = path.resolve(path.dirname(absPath), fragmentPath)
+				const fragmentSource = fs.readFileSync(absFragmentPath).toString()
+				fragmentDefs = [...fragmentDefs, ...gql`${fragmentSource}`.definitions]
+			})
+		},
+		parse () {
+			const parsedAST = gql`${source}`
+			parsedAST.definitions = [...parsedAST.definitions, ...fragmentDefs]
+			ast = parsedAST
+		},
+		get ast () { return ast }
+	}
+}
