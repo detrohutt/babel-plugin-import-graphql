15f04a82ebe55a2bb4d0f22ce040370b08661001
Alexander Roberts <detrohutt@gmail.com>
2017-08-04 01:05:47 -0500

    Add support for nested fragments

--- plugin/index.js
+++ plugin/index.js
@@ -1,13 +1,15 @@
-import path from 'path'
-import os from 'os'
-import fs from 'fs'
-import gql from 'graphql-tag'
+import { dirname, resolve } from 'path'
+import { EOL } from 'os'
+import { readFileSync } from 'fs'
 import { parse } from 'babylon'
+import gql from 'graphql-tag'
+
+const seenNames = {}
 
 export default ({ types: t }) => ({
   visitor: {
     ImportDeclaration: {
-      exit(curPath, state) {
+      exit (curPath, state) {
         const importPath = curPath.node.source.value
         if (importPath.endsWith('.graphql') || importPath.endsWith('.gql')) {
           const query = createQuery(importPath, state.file.opts.filename)
@@ -16,15 +18,15 @@ export default ({ types: t }) => ({
           query.parse()
           query.makeSourceEnumerable()
           curPath.replaceWith(t.variableDeclaration('const', [buildInlineVariable(query.ast)]))
+        }
 
-          function buildInlineVariable (graphqlAST) {
-            const babelAST = parse('const obj = ' + JSON.stringify(graphqlAST))
-            const objExp = babelAST.program.body[0].declarations[0].init
-            return t.variableDeclarator(
-              t.identifier(curPath.node.specifiers[0].local.name),
-              t.objectExpression(objExp.properties)
-            )
-          }
+        function buildInlineVariable (graphqlAST) {
+          const babelAST = parse('const obj = ' + JSON.stringify(graphqlAST))
+          const objExp = babelAST.program.body[0].declarations[0].init
+          return t.variableDeclarator(
+            t.identifier(curPath.node.specifiers[0].local.name),
+            t.objectExpression(objExp.properties)
+          )
         }
       }
     }
@@ -32,26 +34,36 @@ export default ({ types: t }) => ({
 })
 
 function createQuery (queryPath, babelPath) {
-  const absPath = path.resolve(path.dirname(babelPath), queryPath)
-  const source = fs.readFileSync(absPath).toString()
+  const absPath = resolve(dirname(babelPath), queryPath)
+  const source = readFileSync(absPath).toString()
   let ast = null
   let fragmentDefs = []
 
   return {
     processFragments () {
-      const imports = source.split(os.EOL).filter(line => line.startsWith('#import'))
-      imports.forEach(statement => {
-        const fragmentPath = statement.split(' ')[1].slice(1, -1)
-        const absFragmentPath = path.resolve(path.dirname(absPath), fragmentPath)
-        const fragmentSource = fs.readFileSync(absFragmentPath).toString()
-        fragmentDefs = [...fragmentDefs, ...gql`${fragmentSource}`.definitions]
-      })
+      processImports(getImportStatements(source), dirname(absPath))
+
+      function getImportStatements (src) {
+        return src.split(EOL).filter(line => line.startsWith('#import'))
+      }
+
+      function processImports (imports, relDir) {
+        imports.forEach(statement => {
+          const fragmentPath = statement.split(' ')[1].slice(1, -1)
+          const absFragmentPath = resolve(relDir, fragmentPath)
+          const fragmentSource = readFileSync(absFragmentPath).toString()
+          const subFragments = getImportStatements(fragmentSource)
+          if (subFragments.length > 0) {
+            processImports(subFragments, dirname(absFragmentPath))
+          }
+          fragmentDefs = [...gql`${fragmentSource}`.definitions, ...fragmentDefs]
+        })
+      }
     },
     dedupeFragments () {
-      let seenNames = {}
       fragmentDefs = fragmentDefs.filter(def => {
-        if (def.kind !== 'FragmentDefinition') return true;
-        return seenNames[def.name.value] ? false : seenNames[def.name.value] = true
+        if (def.kind !== 'FragmentDefinition') return true
+        return seenNames[def.name.value] ? false : (seenNames[def.name.value] = true)
       })
     },
     parse () {
@@ -59,11 +71,13 @@ function createQuery (queryPath, babelPath) {
       parsedAST.definitions = [...parsedAST.definitions, ...fragmentDefs]
       ast = parsedAST
     },
-    makeSourceEnumerable() {
+    makeSourceEnumerable () {
       const newAST = JSON.parse(JSON.stringify(ast))
       newAST.loc.source = ast.loc.source
       ast = newAST
     },
-    get ast () { return ast }
+    get ast () {
+      return ast
+    }
   }
 }
