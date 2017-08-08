17a791b90fd59ea69fcb33c631692310143002f0
Pierre Martin <pierre@occitech.fr>
2017-08-08 08:14:22 +0200

    Support babel's resolveModuleSource custom resolvers

--- plugin/index.js
+++ plugin/index.js
@@ -4,35 +4,53 @@ import { readFileSync } from 'fs'
 import { parse } from 'babylon'
 import gql from 'graphql-tag'
 
-export default ({ types: t }) => ({
-  visitor: {
-    ImportDeclaration: {
-      enter (curPath, state) {
-        const importPath = curPath.node.source.value
-        if (importPath.endsWith('.graphql') || importPath.endsWith('.gql')) {
-          const query = createQuery(importPath, state.file.opts.filename)
-          query.processFragments()
-          query.parse()
-          query.dedupeFragments()
-          query.makeSourceEnumerable()
-          curPath.replaceWith(t.variableDeclaration('const', [buildInlineVariable(query.ast)]))
-        }
+export default ({ types: t }) => {
+  let sourceResolver;
+  const resolveWithPreviousResolver = (currentFile, fileToImport) => {
+    if (typeof sourceResolver === "undefined") {
+      return resolve(dirname(currentFile), fileToImport);
+    }
+    return sourceResolver(fileToImport, currentFile);
+  };
 
-        function buildInlineVariable (graphqlAST) {
-          const babelAST = parse(`(${JSON.stringify(graphqlAST)})`)
-          const objExp = babelAST.program.body[0].expression
-          return t.variableDeclarator(
-            t.identifier(curPath.node.specifiers[0].local.name),
-            t.objectExpression(objExp.properties)
-          )
+  return {
+    manipulateOptions: function (babelOpts) {
+      sourceResolver = babelOpts.resolveModuleSource;
+      return babelOpts;
+    },
+    visitor: {
+      ImportDeclaration: {
+        enter (curPath, state) {
+          const importPath = curPath.node.source.value
+          if (importPath.endsWith('.graphql') || importPath.endsWith('.gql')) {
+            const query = createQuery(
+              importPath,
+              state.file.opts.filename,
+              resolveWithPreviousResolver
+            )
+            query.processFragments()
+            query.parse()
+            query.dedupeFragments()
+            query.makeSourceEnumerable()
+            curPath.replaceWith(t.variableDeclaration('const', [buildInlineVariable(query.ast)]))
+          }
+
+          function buildInlineVariable (graphqlAST) {
+            const babelAST = parse(`(${JSON.stringify(graphqlAST)})`)
+            const objExp = babelAST.program.body[0].expression
+            return t.variableDeclarator(
+              t.identifier(curPath.node.specifiers[0].local.name),
+              t.objectExpression(objExp.properties)
+            )
+          }
         }
       }
     }
   }
-})
+}
 
-function createQuery (queryPath, babelPath) {
-  const absPath = resolve(dirname(babelPath), queryPath)
+function createQuery (queryPath, babelPath, resolveWithPreviousResolver) {
+  const absPath = resolveWithPreviousResolver(babelPath, queryPath)
   const source = readFileSync(absPath).toString()
   let ast = null
   let fragmentDefs = []
@@ -48,7 +66,7 @@ function createQuery (queryPath, babelPath) {
       function processImports (imports, relDir) {
         imports.forEach(statement => {
           const fragmentPath = statement.split(' ')[1].slice(1, -1)
-          const absFragmentPath = resolve(relDir, fragmentPath)
+          const absFragmentPath = resolveWithPreviousResolver(relDir, fragmentPath)
           const fragmentSource = readFileSync(absFragmentPath).toString()
           const subFragments = getImportStatements(fragmentSource)
           if (subFragments.length > 0) {
