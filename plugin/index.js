93bc669fdc85f591f844a3c6fe15b4d0b21b15df
DoubleOK <oisin@doubleok.com>
2017-12-07 14:10:12 +0100

    undo unecessary styling changes

--- plugin/index.js
+++ plugin/index.js
@@ -7,15 +7,14 @@ import gql from 'graphql-tag'
 let resolve
 
 export default ({ types: t }) => ({
-  manipulateOptions({ resolveModuleSource }) {
+  manipulateOptions ({ resolveModuleSource }) {
     if (!resolve) {
-      resolve =
-        resolveModuleSource || ((src, file) => path.resolve(dirname(file), src))
+      resolve = resolveModuleSource || ((src, file) => path.resolve(dirname(file), src))
     }
   },
   visitor: {
     ImportDeclaration: {
-      exit(curPath, state) {
+      exit (curPath, state) {
         const importPath = curPath.node.source.value
         if (importPath.endsWith('.graphql') || importPath.endsWith('.gql')) {
           const query = createQuery(importPath, state.file.opts.filename)
@@ -26,34 +25,33 @@ export default ({ types: t }) => ({
           curPath.replaceWith(buildInlineVariableAST(query.ast))
         }
 
-        function buildInlineVariableAST(graphqlAST) {
+        function buildInlineVariableAST (graphqlAST) {
           const inlineVarName = curPath.node.specifiers[0].local.name
-          return parse(`const ${inlineVarName} = ${JSON.stringify(graphqlAST)}`)
-            .program.body[0]
+          return parse(`const ${inlineVarName} = ${JSON.stringify(graphqlAST)}`).program.body[0]
         }
       }
     }
   }
 })
 
-function createQuery(queryPath, babelPath) {
+function createQuery (queryPath, babelPath) {
   const absPath = resolve(queryPath, babelPath)
   const source = readFileSync(absPath).toString()
   let ast = null
   let fragmentDefs = []
 
   return {
-    processFragments() {
+    processFragments () {
       processImports(getImportStatements(source), absPath)
 
-      function getImportStatements(src) {
+      function getImportStatements (src) {
         return src
           .replace(/\r/g, '')
           .split(/\n+/g)
           .filter(line => line.startsWith('#import'))
       }
 
-      function processImports(imports, relFile) {
+      function processImports (imports, relFile) {
         imports.forEach(statement => {
           const fragmentPath = statement.split(/[\s\n]+/g)[1].slice(1, -1)
           const absFragmentPath = resolve(fragmentPath, relFile)
@@ -64,12 +62,7 @@ function createQuery(queryPath, babelPath) {
           if (subFragments.length > 0) {
             processImports(subFragments, absFragmentPath)
           }
-          fragmentDefs = [
-            ...gql`
-              ${fragmentSource}
-            `.definitions,
-            ...fragmentDefs
-          ]
+          fragmentDefs = [...gql`${fragmentSource}`.definitions,...fragmentDefs]
         })
       }
     },
@@ -80,21 +73,19 @@ function createQuery(queryPath, babelPath) {
       parsedAST.definitions = [...parsedAST.definitions, ...fragmentDefs]
       ast = parsedAST
     },
-    dedupeFragments() {
+    dedupeFragments () {
       let seenNames = {}
       ast.definitions = ast.definitions.filter(def => {
         if (def.kind !== 'FragmentDefinition') return true
-        return seenNames[def.name.value]
-          ? false
-          : (seenNames[def.name.value] = true)
+        return seenNames[def.name.value] ? false : (seenNames[def.name.value] = true)
       })
     },
-    makeSourceEnumerable() {
+    makeSourceEnumerable () {
       const newAST = JSON.parse(JSON.stringify(ast))
       newAST.loc.source = ast.loc.source
       ast = newAST
     },
-    get ast() {
+    get ast () {
       return ast
     }
   }
